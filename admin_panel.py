2fe54a31cc38b3be90bf0127626049df41e73c9e
KarimovMurodilla <karimovmurodilla15@gmail.com>
2023-06-01 23:39:38 +0500

    added login password

--- admin_panel.py
+++ admin_panel.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from sqladmin import Admin, ModelView
+from sqladmin.authentication import AuthenticationBackend
 
 from utils.db_api.models import (
     User, School, 
@@ -8,10 +12,30 @@ from utils.db_api.models import (
 
 from loader import db
 
-engine = db.get_engine()
+
+class AdminAuth(AuthenticationBackend):
+    async def login(self, request: Request) -> bool:
+        request.session.update({"token": "83288wdhhush7d8773ywqydyuyq"})
+        form = await request.form()
+
+        await db.load()
+        
+        if form['username'] == 'otfiv_admin' and form['password'] == 'otfiv_admin1234':
+            return True
+
+    async def logout(self, request: Request) -> bool:
+        request.session.clear()
+        return True
+
+    async def authenticate(self, request: Request) -> Optional[RedirectResponse]:
+        if not "token" in request.session:
+            return RedirectResponse(request.url_for("admin:login"), status_code=302)
+
 
 app = FastAPI()
-admin = Admin(app, engine)
+engine = db.get_engine()
+authentication_backend = AdminAuth(secret_key="83288wdhhush7d8773ywqydyuyq")
+admin = Admin(app=app, engine=engine, authentication_backend=authentication_backend)
 
 
 class UserAdmin(ModelView, model=User):
@@ -21,6 +45,12 @@ class UserAdmin(ModelView, model=User):
     can_delete = False
     icon = "fa-solid fa-user"
     name_plural = "Foydalanuvchilar"
+    
+    def is_visible(self, request: Request) -> bool:
+        return True
+
+    def is_accessible(self, request: Request) -> bool:
+        return True
 
 
 class SchoolAdmin(ModelView, model=School):
@@ -30,6 +60,12 @@ class SchoolAdmin(ModelView, model=School):
     ]
     icon = "fa-solid fa-university"
     name_plural = "Kasb-hunar maktabi"
+    
+    def is_visible(self, request: Request) -> bool:
+        return True
+
+    def is_accessible(self, request: Request) -> bool:
+        return True
 
    
 class CollegeAdmin(ModelView, model=College):
@@ -39,6 +75,12 @@ class CollegeAdmin(ModelView, model=College):
     ]
     icon = "fa-solid fa-university"
     name_plural = "Kollej"
+    
+    def is_visible(self, request: Request) -> bool:
+        return True
+
+    def is_accessible(self, request: Request) -> bool:
+        return True
 
    
 class TexnikumAdmin(ModelView, model=Texnikum):
@@ -48,6 +90,12 @@ class TexnikumAdmin(ModelView, model=Texnikum):
     ]
     icon = "fa-solid fa-university"
     name_plural = "Texnikum"
+    
+    def is_visible(self, request: Request) -> bool:
+        return True
+
+    def is_accessible(self, request: Request) -> bool:
+        return True
 
 
 class LyceumAdmin(ModelView, model=Lyceum):
@@ -57,7 +105,13 @@ class LyceumAdmin(ModelView, model=Lyceum):
     ]
     icon = "fa-solid fa-university"
     name_plural = "Litsey"
-   
+    
+    def is_visible(self, request: Request) -> bool:
+        return True
+
+    def is_accessible(self, request: Request) -> bool:
+        return True
+
 
 admin.add_view(UserAdmin)
 admin.add_view(SchoolAdmin)
